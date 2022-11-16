cc1791abde6cc11d85db4fe130994b7e02151256
Le Rong Shi <shilerong@gmail.com>
2022-11-16 01:30:46 -0500

    Added profile card

--- client/src/components/Profile/BusinessProfile.tsx
+++ client/src/components/Profile/BusinessProfile.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import FactoryIcon from '@mui/icons-material/Factory'
+import { containerStyle } from '../../utils/sharedStyles'
+import ProfileCard from './ProfileCard'
+
+const items = [
+  {
+    icon: <LocationOnIcon/>,
+    label: 'Location',
+    value: 'Toronto, Ontario'
+  },
+  {
+    icon: <FactoryIcon/>,
+    label: 'Industry',
+    value: 'Food'
+  }
+]
 
 export default function BusinessProfile (): JSX.Element {
   return (
-    <Box>
-
+    <Box component="main" sx={containerStyle.contentBox}>
+      <Container maxWidth="md" sx={containerStyle.contentContainer}>
+        <Grid container>
+          <Grid item xs={12}>
+            <ProfileCard items={items} name="Company Name"/>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   )
 }
--- client/src/components/Profile/InfluencerProfile.tsx
+++ client/src/components/Profile/InfluencerProfile.tsx
@@ -1,8 +1,47 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import { containerStyle } from '../../utils/sharedStyles'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import ProfileCard from './ProfileCard'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import BoyIcon from '@mui/icons-material/Boy'
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp'
+
+const items = [
+  {
+    icon: <LocationOnIcon/>,
+    label: 'Location',
+    value: 'Toronto, Ontario'
+  },
+  {
+    icon: <BoyIcon/>,
+    label: 'Age',
+    value: '12'
+  },
+  {
+    icon: <SubscriptionsIcon/>,
+    label: 'Subscribers',
+    value: '1000'
+  },
+  {
+    icon: <ThumbUpIcon/>,
+    label: 'Likes',
+    value: '1000'
+  }
+]
 
 export default function InfluencerProfile (): JSX.Element {
   return (
-    <Box></Box>
+    <Box component="main" sx={containerStyle.contentBox}>
+      <Container maxWidth="md" sx={containerStyle.contentContainer}>
+        <Grid container>
+          <Grid item xs={12}>
+            <ProfileCard items={items} name="Influencer Name"/>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   )
 }
