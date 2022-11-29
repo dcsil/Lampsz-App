6e41efaac6d206624b78a063e8fd438f788e1d3f
Brian Huang <brianhuang1128@gmail.com>
2022-11-28 23:18:29 -0500

    trim whitespace at end of line for style check

--- client/src/components/Marketplace/index.tsx
+++ client/src/components/Marketplace/index.tsx
@@ -3,47 +3,68 @@ import Box from '@mui/material/Box'
 import MarketplaceTaskCard from '../Shared/MarketplaceTaskCard'
 import Grid from '@mui/material/Grid'
 
+const data = [
+  {
+    title: 'T1',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T2',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T3',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T4',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T5',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T6',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T7',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T8',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T9',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T10',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T11',
+    description: 'asdfassdfasdf'
+  },
+  {
+    title: 'T12',
+    description: 'asdfassdfasdf'
+  }
+]
 
 export default function Marketplace (): JSX.Element {
   return (
     <Box sx={{ display: 'flex' }}>
         <Grid container spacing={5}>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T1' description='Card for T1' height = '300' />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T2' description='Card for T2' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T3' description='Card for T3' height = '600'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4} >
-                <MarketplaceTaskCard title='T4' description='Card for T4' height = '800'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T2' description='Card for T2' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T3' description='Card for T3' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4} >
-                <MarketplaceTaskCard title='T1' description='Card for T1' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T2' description='Card for T2' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T3' description='Card for T3' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4} >
-                <MarketplaceTaskCard title='T1' description='Card for T1' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T2' description='Card for T2' height = '240'/>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
-                <MarketplaceTaskCard title='T3' description='Card for T3' height = '240'/>
-            </Grid>
+            {data.map((item, index) =>
+            (
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2.4}>
+                    <MarketplaceTaskCard title={item.title} description={item.description} height = '300' />
+                </Grid>
+            )
+            )}
         </Grid>
     </Box>
   )
