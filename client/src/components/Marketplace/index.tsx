e4333de9f034f6c0242557642eb54b2186a65ed7
Brian Z Huang <brianhuang1128@gmail.com>
2022-12-04 02:17:46 -0500

    Add search bar (#60)
    * add search bar
    
    * add imports and update package-lock
    
    * Changed json response for backend yt and included all metrics to be r… (#61)
    
    * Changed json response for backend yt and included all metrics to be real time
    
    * Changed json response for backend yt and included all metrics to be real time
    
    * Update ci.yml
    
    * Changed json response for backend yt and included all metrics to be real time
    
    * Changed json response for backend yt and included all metrics to be real time
    
    * fix comments
    
    * add search bar
    
    * add imports and update package-lock
    
    * fix comments
    
    * add Response import to profile.py
    
    Co-authored-by: Matthew Yang <50401897+matticyang@users.noreply.github.com>

--- client/src/components/Marketplace/index.tsx
+++ client/src/components/Marketplace/index.tsx
@@ -7,20 +7,22 @@ import { MarketingTask } from '../../utils/types'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { containerStyle } from '../../utils/utils'
+import { SearchBar } from '../Shared/SearchBar'
 
 export default function Marketplace (): JSX.Element {
   const auth = useAuth()
   const [tasks, setTasks] = useState<MarketingTask[]>([])
-
+  const [ query, setQuery ] = useState("")
   useEffect(() => {
     getTasks(auth.userId, setTasks)
   }, [])
 
   return (
-    <Container component="main" maxWidth="lg" sx={containerStyle.contentContainer}>
+    <Container component="main" maxWidth={false} sx={{width: '90%'}}>
       <Box display="flex" sx={{ mb: 3 }}>
         <Typography variant="h4" gutterBottom>Marketplace</Typography>
       </Box>
+      <SearchBar setTasks={setTasks}></SearchBar>
       <TasksBox tasks={tasks}></TasksBox>
     </Container>
 
