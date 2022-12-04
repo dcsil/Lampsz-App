713b452a8e877215f1a4c32df86f3136aef45fc2
Le Rong Shi <shilerong@gmail.com>
2022-12-03 19:36:49 -0500

    improved styling

--- client/src/components/MyMarketingTasks/index.tsx
+++ client/src/components/MyMarketingTasks/index.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@mui/material'
+import { Container, Stack } from '@mui/material'
 import TasksBox from '../Shared/TasksBox'
 import React, { useEffect, useState } from 'react'
 import useAuth from '../../hooks/AuthHook'
@@ -6,6 +6,9 @@ import MarketingTaskForm from './MarketingTaskForm'
 import { getTasks } from '../../actions/tasks'
 import { MarketingTask } from '../../utils/types'
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { containerStyle } from '../../utils/utils'
 
 export default function MyMarketingTasks (): JSX.Element {
   const auth = useAuth()
@@ -15,10 +18,16 @@ export default function MyMarketingTasks (): JSX.Element {
   useEffect(() => getTasks(auth.userId, setTasks), [])
 
   return (
-    <Container component="main" maxWidth="lg">
-      <Button variant="outlined" onClick={() => setOpen(true)}>
-        Create New Task
-      </Button>
+    <Container component="main" maxWidth="lg" sx={containerStyle.contentContainer}>
+      <Box display="flex" justifyContent="space-between" sx={{ mb: 3 }}>
+        <Box>
+          <Typography variant="h4" gutterBottom>My Marketing Tasks</Typography>
+        </Box>
+        <Stack p={1} spacing={1}>
+          <Button variant="outlined" onClick={() => setOpen(true)}>Create New Task</Button>
+        </Stack>
+      </Box>
+
       <MarketingTaskForm open={open} closeDialog={() => setOpen(false)} isCreate={true}/>
       <TasksBox tasks={tasks}></TasksBox>
     </Container>
