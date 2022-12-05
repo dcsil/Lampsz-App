bc6f364536ba8257a6095d3e3b7ab4f065adf32e
Le Rong Shi <shilerong@gmail.com>
2022-12-04 23:10:32 -0500

    Update index.tsx

--- client/src/components/Marketplace/index.tsx
+++ client/src/components/Marketplace/index.tsx
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { getTasks } from '../../actions/marketingTask'
 import { SearchBar } from '../Shared/SearchBar'
+import { containerStyle } from '../../utils/utils'
 
 export default function Marketplace (): JSX.Element {
   const auth = useAuth()
@@ -16,7 +17,7 @@ export default function Marketplace (): JSX.Element {
   useEffect(() => getTasks(auth.userId, setTasks), [])
 
   return (
-    <Container component="main" maxWidth={false} sx={{ width: '90%' }}>
+    <Container component="main" maxWidth="lg" sx={containerStyle.contentContainer}>
       <Box display="flex" sx={{ mb: 3 }}>
         <Typography variant="h4" gutterBottom>Marketplace</Typography>
       </Box>
