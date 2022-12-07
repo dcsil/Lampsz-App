6d827f51623107bed50adb3adf9ca746b4ab3e81
Brian Z Huang <brianhuang1128@gmail.com>
2022-12-06 19:02:42 -0500

    Initial populate marketplace (#72)
    * if user_id is 0, return everything
    
    * send user id 0 to populate marketplace with all tasks
    
    * change zero check to string
    
    * package lock:
    
    * remove print statements
    
    * remove privary policy and terms and conditions from frontend

--- lampsz/frontend/urls.py
+++ lampsz/frontend/urls.py
@@ -16,14 +16,3 @@ urlpatterns = [
     path(pattern, TemplateView.as_view(template_name="frontend/index.html"))
     for pattern in urls
 ]
-
-urlpatterns += [
-    path(
-        "terms-and-conditions",
-        TemplateView.as_view(template_name="frontend/terms_and_conditions.html"),
-    ),
-    path(
-        "privacy-policy",
-        TemplateView.as_view(template_name="frontend/privacy_policies.html"),
-    ),
-]
