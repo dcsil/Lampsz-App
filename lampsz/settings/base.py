3084cfd98f9d5fe6368042cf0cc6e7d70c164c56
Brian Huang <brianhuang1128@gmail.com>
2022-12-08 00:05:30 -0500

    try to fix media folder bug on production

--- lampsz/settings/base.py
+++ lampsz/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -154,4 +155,6 @@ LOGGING = {
 
 
 MEDIA_ROOT = BASE_DIR / "media"
+if not os.path.exists(MEDIA_ROOT):
+    os.mkdir(MEDIA_ROOT)
 MEDIA_URL = "/media/"
