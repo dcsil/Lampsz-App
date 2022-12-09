73587fe6612c433832daf9adf3b197500f397eeb
Le Rong Shi <shilerong@gmail.com>
2022-12-08 22:35:08 -0500

    fixed influencer age can't be edited

--- client/src/components/Profile/index.tsx
+++ client/src/components/Profile/index.tsx
@@ -4,7 +4,7 @@ import ProfileBase from './ProfileBase'
 
 export default function Profile (): JSX.Element {
   const data = useLoaderData() as any
-  const influencerItems = ['ShortBio', 'Description']
+  const influencerItems = ['ShortBio', 'Description', 'Age']
   const businessItems = ['ShortBio', 'Location', 'Industry', 'Description']
 
   return data.user.isInfluencer ? <ProfileBase items={influencerItems}/> : <ProfileBase items={businessItems}/>
